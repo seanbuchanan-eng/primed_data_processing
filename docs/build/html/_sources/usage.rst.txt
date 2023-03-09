b0e61ab95d698da867c8fd9157838836b6d7ce3d
seanbuchanan-eng <seanbuchanan@uvic.ca>
2023-03-09 15:50:24 -0800

    add auto documentation

---
+++ docs/build/html/_sources/usage.rst.txt
@@ -0,0 +1,11 @@
+Usage
+=====
+
+Installation
+------------
+
+To use primed_data_processing, first install it using pip:
+
+.. code-block:: console
+
+    pip install primed_data_processing
---
+++ docs/source/conf.py
@@ -0,0 +1,38 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import sys, os
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../src'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Primed Data Processing'
+copyright = '2023, Sean Buchanan'
+author = 'Sean Buchanan'
+release = '0.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinxawesome_theme',
+]
+html_collapsible_definitions = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinxawesome_theme'
+html_static_path = ['_static']
